e8475081aa0421ac051744ef386bccdda5a1c1c5
fazle-mubin-bjit <131145969+fazle-mubin-bjit@users.noreply.github.com>
2023-05-25 08:17:55 +0600

    Update connectDB.php changed to pdo again

--- bookstore/connectDB.php
+++ bookstore/connectDB.php
@@ -1,21 +1,3 @@
 <?php
-$servername = "192.168.20.158"; // Replace with your server name
-$username = "root"; // Replace with your MySQL username
-$password = ""; // Replace with your MySQL password
-$database = "bookstore"; // Replace with the name of your MySQL database
-
-// Create a connection object
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-echo "Connected successfully";
-
-// Perform your database operations here
-
-// Close the connection
-$conn->close();
+$pdo=new PDO('mysql:host=192.168.20.158;port=3306;dbname=bookstore','root', '');
 ?>
